759a4d6fb543e31ab15067453e23ea7f974a137b
Chase Weaver <chase.weaver34@gmail.com>
2018-02-26 03:43:12 -0500

    Cleaned up code

--- commands/Music/add.js
+++ commands/Music/add.js
@@ -1,5 +1,7 @@
-const yt = require('ytdl-core');
-const getInfoAsync = require('util').promisify(yt.getInfo);
+/* Adds a YouTube 'song' to the queue. */
+
+const ytdl = require('ytdl-core');
+const getInfoAsync = require('util').promisify(ytdl.getInfo);
 
 const exp = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/\S*(?:(?:\/e(?:mbed)?)?\/|watch\/?\?(?:\S*?&?v=))|youtu\.be\/)([\w-]{11})(?:[^\w-]|$)/;
 
@@ -8,7 +10,7 @@ exports.run = async (client, msg, [song]) => {
   if (id === null) throw 'You must provide a valid YouTube URL.';
   const info = await getInfoAsync(`https://youtu.be/${id[1]}`);
 
-  if (client.queue.has(msg.guild.id) === false) {
+  if (!client.queue.has(msg.guild.id)) {
     client.queue.set(msg.guild.id, {
       playing: false,
       songs: [],
