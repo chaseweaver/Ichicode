c2b6ecc00af3806c6a30625ebe6b0e7587cd2659
Chase Weaver <chase.weaver34@gmail.com>
2018-02-26 06:40:01 -0500

    Updated to 12.0

--- commands/Bot/presence.js
+++ commands/Bot/presence.js
@@ -5,19 +5,19 @@ exports.run = async (client, msg, [type, status = 'online', ...str]) => {
   switch (type) {
   case 'status':
     await client.user.setStatus(status);
-    return msg.channel.send(`Status changed to ***${status}***`)
+    return msg.send(`Status changed to ***${status}***`)
       .then(m => m.delete(5000)).then(() => msg.delete()).catch(console.error);
   case 'watching':
     await client.user.setActivity(str, { type: 'WATCHING' });
-    return msg.channel.send(`${str ? `Watching changed to ***${str}***` : 'Watching cleared'}`)
+    return msg.send(`${str ? `Watching changed to ***${str}***` : 'Watching cleared'}`)
       .then(m => m.delete(5000)).then(() => msg.delete()).catch(console.error);
   case 'listening':
     await client.user.setActivity(str, { type: 'LISTENING' });
-    return msg.channel.send(`${str ? `Listening changed to ***${str}***` : 'Listening cleared'}`)
+    return msg.send(`${str ? `Listening changed to ***${str}***` : 'Listening cleared'}`)
       .then(m => m.delete(5000)).then(() => msg.delete()).catch(console.error);
   default:
-    await client.user.setGame(str);
-    return msg.channel.send(`${str ? `Game changed to ***${str}***` : 'Game cleared'}`)
+    await client.user.setActivity(str);
+    return msg.send(`${str ? `Game changed to ***${str}***` : 'Game cleared'}`)
       .then(m => m.delete(5000)).then(() => msg.delete()).catch(console.error);
   }
 };
