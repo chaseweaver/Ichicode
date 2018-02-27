29a683c7541049c59a69ba004339154b6b59468c
Chase Weaver <chase.weaver34@gmail.com>
2018-02-26 23:12:43 -0500

    Updated try

--- events/message.js
+++ events/message.js
@@ -1,30 +1,30 @@
 exports.run = (client, msg) => {
-
+  if (msg.author.bot) return;
   /* Put custom stuff here for tripping */
 
-  /* This deletes messages that get spammed into chat */
-  if (msg.author.bot) return;
-  if (!msg.guild.settings.antiSpam || !msg.guild.settings.cooldown) return;
-  if (msg.guild.settings.antiSpam && msg.guild.settings.cooldown) {
-    if (msg.member.roles.find('name', msg.guild.settings.modRole) ||
-      msg.member.roles.find('name', msg.guild.settings.adminRole) ||
-      (msg.member.id === client.ownerID)) return;
-    if (msg.channel.type === 'dm') return;
-    const cooldown = msg.guild.settings.cooldown;
-    const msgTS = msg.createdTimestamp;
-    const data = [];
-    msg.channel.messages.fetch({ limit: 15 })
-      .then(m => {
-        const arr = m.array();
-        for (let i = 0; i < arr.length; i++) {
-          if (msg.author.id === arr[i].author.id) {data.push(arr[i].createdTimestamp);}
-          if (data.length >= 2) return;
-        }
-      })
-      .then(function() {
-        const oldTS = data[1];
-        if (msgTS <= oldTS + (cooldown * 1000)) msg.delete();
-      })
-      .catch(console.error);
-  }
+  try {
+    /* This deletes messages that get spammed into chat */
+    if (msg.guild.settings.antiSpam && msg.guild.settings.cooldown) {
+      if (msg.member.roles.find('name', msg.guild.settings.modRole) ||
+        msg.member.roles.find('name', msg.guild.settings.adminRole) ||
+        (msg.member.id === client.ownerID)) return;
+      if (msg.channel.type === 'dm') return;
+      const cooldown = msg.guild.settings.cooldown;
+      const msgTS = msg.createdTimestamp;
+      const data = [];
+      msg.channel.messages.fetch({ limit: 15 })
+        .then(m => {
+          const arr = m.array();
+          for (let i = 0; i < arr.length; i++) {
+            if (msg.author.id === arr[i].author.id) {data.push(arr[i].createdTimestamp);}
+            if (data.length >= 2) return;
+          }
+        })
+        .then(function() {
+          const oldTS = data[1];
+          if (msgTS <= oldTS + (cooldown * 1000)) msg.delete();
+        })
+        .catch(console.error);
+    }
+  } catch (error) { console.log(error); }
 };
