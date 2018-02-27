04165fca02a53881ec9eaa81459ca344ccc06b47
Chase Weaver <chase.weaver34@gmail.com>
2018-02-27 06:48:43 -0500

    Added role permissions

--- Ichicode_Example.js
+++ Ichicode_Example.js
@@ -1,8 +1,37 @@
-const komada = require('komada');
+const { Client, PermLevels } = require('komada');
 
-const client = new komada.Client({
+const permStructure = new PermLevels()
+  .addLevel(0, false, () => true)
+  .addLevel(2, false, (client, msg) => {
+    /* Dev + Mod + Admin */
+    if (!msg.guild || !msg.guild.settings.adminRole || !msg.get.settings.modRole || !msg.get.settings.devRole) return false;
+    const adminRole = msg.guild.roles.get(msg.guild.settings.adminRole);
+    const modRole = msg.guild.roles.get(msg.guild.settings.modRoles);
+    const devRole = msg.guild.roles.get(msg.get.settings.devRole);
+    return ((adminRole && msg.member.roles.has(adminRole.id)) || (modRole && msg.member.roles.has(modRole.id))
+      || (devRole && msg.member.roles.has(devRole.id)));
+  })
+  .addLevel(3, false, (client, msg) => {
+    /* Mod + Admin */
+    if (!msg.guild || !msg.guild.settings.adminRole || !msg.get.settings.modRole) return false;
+    const adminRole = msg.guild.roles.get(msg.guild.settings.adminRole);
+    const modRole = msg.guild.roles.get(msg.guild.settings.modRoles);
+    return ((adminRole && msg.member.roles.has(adminRole.id)) || (modRole && msg.member.roles.has(modRole.id)));
+  })
+  .addLevel(4, false, (client, msg) => {
+    /* Admin */
+    if (!msg.guild || !msg.guild.settings.adminRole) return false;
+    const adminRole = msg.guild.roles.get(msg.guild.settings.adminRole);
+    return ((adminRole && msg.member.roles.has(adminRole.id)));
+  })
+  .addLevel(4, false, (client, msg) => msg.guild && msg.author.id === msg.guild.owner.id)
+  .addLevel(9, true, (client, msg) => msg.author.id === client.config.ownerID)
+  .addLevel(10, false, (client, msg) => msg.author.id === client.config.ownerID);
+
+const client = new Client({
   ownerID : 'OWNER_ID',
   prefix: '+',
+  permStructure,
   clientOptions: {
     fetchAllMembers: false,
     sync: true,
--- inhibitors/cooldown.js
+++ inhibitors/cooldown.js
@@ -3,7 +3,7 @@ exports.run = (client, msg, cmd) => {
   const cooldown = cmd.conf.cooldown || 1000;
   if (!cmd.cooldown) cmd.cooldown = new Map();
   const remaining = (cooldown - (Date.now() - cmd.cooldown.get(msg.author.id))) / 1000;
-  if (cmd.cooldown.has(msg.author.id)) return `You are being ratelimited. Wait ${remaining}s before trying again!`;
+  if (cmd.cooldown.has(msg.author.id)) return `You need to chill. Wait ${remaining}s before trying again!`;
   cmd.cooldown.set(msg.author.id, Date.now());
   setTimeout(() => cmd.cooldown.delete(msg.author.id), cooldown);
   return false;
