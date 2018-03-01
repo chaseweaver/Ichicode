3df8d7d6881226857c848087942338486b92b950
Chase Weaver <chase.weaver34@gmail.com>
2018-03-01 00:05:36 -0500

    Added option to list all role counts

--- commands/stats/rolemembercount.js
+++ commands/stats/rolemembercount.js
@@ -1,23 +1,39 @@
 /* Returns the member count per given role. */
 
-exports.run = async (client, msg, role) => {
+exports.run = async (client, msg, [role]) => {
   let str = `[${msg.guild.nameAcronym}] ${msg.guild.name}\n\n`;
-  const roltmp = role.join(' ');
+  let roltmp;
+  if (role) roltmp = role.join(' ');
   let found = false;
-  msg.guild.roles.find(rol => {
-    if (rol.name === roltmp) {
-      str += `> ${rol.name} : ${rol.members.size} members\n`;
-      found = true;
-    }
-  });
-  if (!found) return msg.reply('I did not find that role!').catch(err => console.log(err, 'error'));
-  return msg.send(str, { code: 'xl' }).catch(err => console.log(err, 'error'));
+
+  if (role) {
+    msg.guild.roles.find(rol => {
+      if (rol.name === roltmp) {
+        str += `> ${rol.name} : ${rol.members.size} members\n`;
+        found = true;
+      }
+    });
+    if (!found) return msg.reply('I did not find that role!').catch(err => console.log(err, 'error'));
+    return msg.send(str, { code: 'xl' }).catch(err => console.log(err, 'error'));
+  } else {
+    let itr = 1;
+    let plc = '';
+    msg.guild.roles.array().forEach(rol => {
+      if (rol.members.size <= 9) plc = '   ';
+      if (rol.members.size >= 10 && rol.members.size <= 99) plc = '  ';
+      if (rol.members.size >= 100 && rol.members.size < 1000) plc = ' ';
+      if (itr <= 9) str += `[0${itr}] ${plc}${rol.members.size} | ${rol.name}\n`;
+      else str += `[${itr}] ${plc}${rol.members.size} | ${rol.name}\n`;
+      itr++;
+    });
+    return msg.send(str, { code: 'xl' }).catch(err => console.log(err, 'error'));
+  }
 };
 
 exports.conf = {
   enabled: true,
   runIn: ['text'],
-  aliases: [],
+  aliases: ['roles'],
   permLevel: 2,
   botPerms: [],
   requiredFuncs: [],
@@ -28,7 +44,7 @@ exports.conf = {
 exports.help = {
   name: 'rolemembercount',
   description: 'Gets member count per role.',
-  usage: '<role:str>',
+  usage: '[role:str]',
   usageDelim: '',
   extendedHelp: '',
 };
