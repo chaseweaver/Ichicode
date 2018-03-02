53536340aaf306a82a94c268ef2267078015cd71
Chase Weaver <chase.weaver34@gmail.com>
2018-03-02 04:28:04 -0500

    Initial commit

---
+++ config.ex.js
@@ -0,0 +1,3 @@
+exports.master = 'YOUR MASTER ID HERE';
+exports.token = 'BOT TOKEN HERE';
+exports.ytapi = 'YOUTUBE API HERE';
--- config_ex.js
+++
@@ -1,3 +0,0 @@
-exports.master = 'OWNER_ID';
-exports.token = 'BOT_TOKEN';
-exports.ytapi = 'YOUTUBE_API_KEY';
