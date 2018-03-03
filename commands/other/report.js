90fbc9b625e1c19d5fddfd719782bfa08c802a47
Chase Weaver <chase.weaver34@gmail.com>
2018-03-03 00:29:22 -0500

    Initial commit

---
+++ commands/other/report.js
@@ -0,0 +1,27 @@
+/* Sends a report to error channel. */
+
+exports.run = async (client, msg, [...issue]) => {
+  if (!msg.guild.settings.reportChan) return msg.chan('The report channel is not configured! Bug a mod!');
+  const chan = msg.guild.settings.reportChan;
+  return chan.send(issue)
+    .catch(err => console.log(err, `${msg.author.tag} / ${msg.author.id}\n${msg.guild.name} / ${msg.guild.id}\n${msg.guild.channel.name} / ${msg.guild.channel.id}\n${new Date()}\`\`\`${issue}\`\`\``));
+};
+
+exports.conf = {
+  enabled: true,
+  runIn: ['text', 'dm', 'group'],
+  aliases: [],
+  permLevel: 0,
+  botPerms: [],
+  requiredFuncs: [],
+  cooldown: 30,
+  nsfw: false,
+};
+
+exports.help = {
+  name: 'report',
+  description: 'Sends a report to error channel.',
+  usage: '<issue:str>',
+  usageDelim: '',
+  extendedHelp: '',
+};
