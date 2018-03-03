9b14799c96f9e62edb69027e5c12ad86a4689417
Chase Weaver <chase.weaver34@gmail.com>
2018-03-03 00:50:26 -0500

    Initial commit

--- commands/other/report.js
+++ commands/other/report.js
@@ -2,9 +2,9 @@
 
 exports.run = async (client, msg, [...issue]) => {
   if (!msg.guild.settings.reportChan) return msg.chan('The report channel is not configured! Bug a mod!');
-  const chan = msg.guild.settings.reportChan;
-  return chan.send(`${msg.author.tag} / ${msg.author.id}\n${msg.guild.name} / ${msg.guild.id}\n${msg.guild.channel.name} / ${msg.guild.channel.id}\n${new Date()}\`\`\`${issue}\`\`\``)
-    .catch(err => console.log(err, `${msg.author.tag} / ${msg.author.id}\n${msg.guild.name} / ${msg.guild.id}\n${msg.guild.channel.name} / ${msg.guild.channel.id}\n${new Date()}\`\`\`${issue}\`\`\``));
+  const chan = msg.guild.channels.find('id', msg.guild.settings.reportChan);
+  return chan.send(`${msg.author} / ${msg.guild.id}\n${msg.channel} / ${msg.channel.id}\n${new Date()}\n\n\`\`\`${issue}\`\`\``)
+    .catch(err => console.log(err));
 };
 
 exports.conf = {
