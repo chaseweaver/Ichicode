1f92bfa8577aca285ec5a7c62e0c71e47264816d
Chase Weaver <chase.weaver34@gmail.com>
2018-03-06 08:38:47 -0500

    Initial commit

--- commands/Music/lyrics.js
+++ commands/Music/lyrics.js
@@ -1,4 +1,5 @@
 const { Command } = require('klasa');
+const getLyrics = require('lyric-get');
 
 module.exports = class extends Command {
   constructor(...args) {
@@ -14,13 +15,16 @@ module.exports = class extends Command {
       requiredConfigs: [],
       description: 'Fetches lyrics from a song.',
       quotedStringSupport: true,
-      usage: '<song:str>',
-      usageDelim: '',
+      usage: '<song:str> <artist:str>',
+      usageDelim: ' by ',
       extendedHelp: 'No extended help available.',
     });
   }
 
-  async run(msg, [song]) {
-    console.log(song);
+  async run(msg, [song, artist]) {
+    getLyrics.get(artist, song, function(err, res) {
+      if (err) msg.send('I could not find the source! Perhaps try to rephrase?');
+      else msg.send(`${artist} - ${song}\n\n\n` + res, { code: true, split: true });
+    });
   }
 };
