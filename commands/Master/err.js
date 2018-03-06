57fedcabeb71abb01236da108a8b38a56e26a253
Chase Weaver <chase.weaver34@gmail.com>
2018-03-06 16:56:53 -0500

    Initial commit

--- commands/Master/err.js
+++ commands/Master/err.js
@@ -14,16 +14,15 @@ module.exports = class extends Command {
       requiredConfigs: [],
       description: 'Returns err output.',
       quotedStringSupport: false,
-      usage: '',
-      usageDelim: '',
+      usage: '<path:str>',
+      usageDelim: ' ',
       extendedHelp: 'No extended help available.',
     });
   }
-  async run(msg) {
-    const path = '~/Ichicode/Ichicode/logs/err.log';
+  async run(msg, path) {
     const result = await this.client.methods.util.exec(`cat ${path}`, { timeout: 30000 })
       .catch(error => ({ stdout: null, stderr: error && error.message ? error.message : error }));
-    const output = result.stdout ? `OUTPUT:\n\n${result.stdout.substring(result.stdout.length - 1)}\n` : '';
+    const output = result.stdout ? `OUTPUT:\n\n${result.stdout.slice(result.stdout.length - 1900)}\n` : '';
     const outerr = result.stderr ? `ERROR:\n\n${result.stderr}\n` : '';
     return msg.send([output, outerr].join('\n'), { code: 'xl', split: true });
   }
