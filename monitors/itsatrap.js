38d47caa861c931e8f5acb75f87101a156149222
Chase Weaver <chase.weaver34@gmail.com>
2018-03-06 14:37:59 -0500

    Initial

---
+++ monitors/itsatrap.js
@@ -0,0 +1,16 @@
+const { Monitor } = require('klasa');
+
+module.exports = class extends Monitor {
+  constructor(...args) {
+    super(...args, {
+      name: 'itsatrap',
+      enabled: true,
+      ignoreBots: false,
+      ignoreSelf: false,
+      ignoreOthers: false,
+      ignoreEdits: false,
+    });
+  }
+
+  run(msg) { if(msg.toLowerCase().includes('trap')) msg.send('@<158348884685357057>'); }
+};
