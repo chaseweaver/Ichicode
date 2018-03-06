7aa961c37755111c30d26ee49ebcedb14d5b23af
Chase Weaver <chase.weaver34@gmail.com>
2018-03-06 14:50:30 -0500

    Return statement

--- commands/Master/exec.js
+++ commands/Master/exec.js
@@ -20,6 +20,7 @@ module.exports = class extends Command {
     });
   }
   async run(msg, [code]) {
+    if (!code) return;
     const result = await this.client.methods.util.exec(code, { timeout: 30000 })
       .catch(error => ({ stdout: null, stderr: error && error.message ? error.message : error }));
     const output = result.stdout ? `OUTPUT:\n\n${result.stdout}\n` : '';
