0503339dec5403a78564be91728b32ed77cdb6d2
chaseweaver <chase.weaver34@gmail.com>
2018-03-14 18:21:53 -0400

    Squashed nulls

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -24,8 +24,8 @@ module.exports = class extends Event {
       }
 
       if (mem.guild.configs.logMemberAdd && mem.guild.configs.memberLogChannel) {
-        const chan = mem.channels.find('id', mem.guild.configs.memberLogChannel);
-        if (!chan) return;
+        const memChan = mem.channels.find('id', mem.guild.configs.memberLogChannel);
+        if (!memChan) return;
         const avatar = mem.user.displayAvatarURL() ? mem.user.displayAvatarURL() : mem.guild.iconURL();
         const embed = new this.client.methods.Embed()
           .setColor('#00ffbb')
@@ -34,7 +34,7 @@ module.exports = class extends Event {
           .setAuthor(`${mem.user.tag} / ${mem.user.id}`, avatar)
           .addField('Joined At', `${Moment.utc(mem.joinedTimestamp).format('llll')} UTC-0`)
           .setTimestamp(new Date());
-        chan.send({ embed }).catch(err => console.log(err, 'error'));
+        memChan.send({ embed }).catch(err => console.log(err, 'error'));
       }
     } catch (error) { console.log(error); }
     console.log(`Member '${mem.user.tag}' joined '${mem.guild.name}'. Account age '${mem.user.createdAt}'.`);
