b537e94c0204081fae16a569c9da00dfd0985b87
chaseweaver <chase.weaver34@gmail.com>
2018-03-14 18:55:11 -0400

    Fixed null

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -24,6 +24,8 @@ module.exports = class extends Event {
       }
 
       if (mem.guild.configs.logMemberAdd && mem.guild.configs.memberLogChannel) {
+        const memChan = mem.guild.channels.find('id', mem.guild.configs.memberLogChannel);
+        if (!memChan) return;
         const avatar = mem.user.displayAvatarURL() ? mem.user.displayAvatarURL() : mem.guild.iconURL();
         const embed = new this.client.methods.Embed()
           .setColor('#00ffbb')
@@ -32,7 +34,7 @@ module.exports = class extends Event {
           .setAuthor(`${mem.user.tag} / ${mem.user.id}`, avatar)
           .addField('Joined At', `${Moment.utc(mem.joinedTimestamp).format('llll')} UTC-0`)
           .setTimestamp();
-          mem.guild.configs.memberLogChannel.send({ embed }).catch(err => console.log(err, 'error'));
+        memChan.send({ embed }).catch(err => console.log(err, 'error'));
       }
     } catch (error) { console.log(error); }
     console.log(`Member '${mem.user.tag}' joined '${mem.guild.name}'. Account age '${mem.user.createdAt}'.`);
