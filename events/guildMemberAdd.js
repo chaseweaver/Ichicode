53133a61c156802263a7c24a500b0bf46684e005
chaseweaver <chase.weaver34@gmail.com>
2018-03-14 17:36:35 -0400

    Updated Log

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -13,19 +13,17 @@ module.exports = class extends Event {
 
   run(mem) {
     try {
-      /* Send welcome message if enabled. */
       if (mem.guild.configs.welcomeMessgage && mem.guild.configs.welcomeMemberActive && mem.guild.configs.welcomeChannel) {
         if (!mem.guild.configs.welcomeMemberActive) return;
         let build = `Welcome, ${mem.user.tag}!`;
         const chan = mem.guild.channels.find('id', mem.guild.configs.welcomeChannel);
         const message = mem.guild.configs.welcomeMessgage;
-        if (message.search('$USER$')) build = message.replace('$USER$', mem.user);
+        if (message.search('$MENTION$')) build = message.replace('$MENTION$', mem.user);
+        if (message.search('$SERVER$')) build = message.replace('$SERVER$', mem.guild);
         chan.send(build).catch(err => console.log(err, 'error'));
       }
 
-      /* Log member joined if enabled. */
       if (mem.guild.configs.logMemberAdd && mem.guild.configs.memberLogChannel) {
-        const chan = mem.guild.channels.find('id', mem.guild.configs.memberLogChannel);
         const avatar = mem.user.displayAvatarURL() ? mem.user.displayAvatarURL() : mem.guild.iconURL();
 
         const embed = new this.client.methods.Embed()
@@ -35,7 +33,7 @@ module.exports = class extends Event {
           .setAuthor(`${mem.user.tag} / ${mem.user.id}`, avatar)
           .addField('Joined At', `${Moment.utc(mem.joinedTimestamp).format('llll')} UTC-0`)
           .setTimestamp(new Date());
-        chan.send({ embed }).catch(err => console.log(err, 'error'));
+        mem.guild.configs.memberLogChannel.send({ embed }).catch(err => console.log(err, 'error'));
       }
     } catch (error) { console.log(error); }
     console.log(`Member '${mem.user.tag}' joined '${mem.guild.name}'. Account age '${mem.user.createdAt}'.`);
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -13,19 +13,17 @@ module.exports = class extends Event {
 
   run(mem) {
     try {
-      /* Send goodbye message if enabled. */
       if (mem.guild.configs.goodbyeMessgage && mem.guild.configs.goodbyeMemberActive && mem.guild.configs.goodbyeChannel) {
         if (!mem.guild.configs.goodbyeMemberActive) return;
         let build = `Welcome, ${mem.user.tag}!`;
         const chan = mem.guild.channels.find('id', mem.guild.configs.goodbyeChannel);
         const message = mem.guild.configs.goodbyeMessage;
-        if (message.search('$USER$')) build = message.replace('$USER$', mem.user);
+        if (message.search('$MENTION$')) build = message.replace('$MENTION$', mem.user);
+        if (message.search('$SERVER$')) build = message.replace('$SERVER$', mem.guild);
         chan.send(build).catch(err => console.log(err, 'error'));
       }
 
-      /* Log member removed if enabled. */
       if (mem.guild.configs.logMemberRemove && mem.guild.configs.memberLogChannel) {
-        const chan = mem.guild.channels.find('id', mem.guild.configs.memberLogChannel);
         const avatar = mem.user.displayAvatarURL() ? mem.user.displayAvatarURL() : mem.guild.iconURL();
 
         const embed = new this.client.methods.Embed()
@@ -37,7 +35,7 @@ module.exports = class extends Event {
           .addField('Left At', `${Moment.utc(new Date()).format('llll')} UTC-0`)
           .addField('Account Age', `${Moment.utc(mem.user.createdAt).format('llll')} UTC-0`)
           .setTimestamp(new Date());
-        chan.send({ embed }).catch(err => console.log(err, 'error'));
+        mem.guild.configs.memberLogChannel.send({ embed }).catch(err => console.log(err, 'error'));
       }
     } catch (error) { console.log(error); }
     console.log(`Member '${mem.user.tag}' left '${mem.guild.name}'. Account age '${mem.user.createdAt}'.`);
