839ec2b9a4d8537127d2771dfc6b5eab2c814d31
chaseweaver <chase.weaver34@gmail.com>
2018-03-16 21:56:35 -0400

    Initial

---
+++ commands/Misc/shameban.js
@@ -0,0 +1,24 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      name: 'shameban',
+      enabled: true,
+      runIn: ['text', 'dm', 'group'],
+      cooldown: 2,
+      bucket: 1,
+      aliases: [],
+      permLevel: 3,
+      botPerms: [],
+      requiredConfigs: [],
+      description: 'Shame bans a user.',
+      quotedStringSupport: true,
+      usage: '<member:member>',
+      usageDelim: '',
+      extendedHelp: 'No extended help available.',
+    });
+  }
+
+  async run(msg, [member]) { msg.send(`🔨 BAN 🔨 ${member} 🔨 BAN 🔨`).then(() => msg.delete()); }
+};
