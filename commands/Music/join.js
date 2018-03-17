e19e8af0b83333f5843a67b6bf28f44ffcf178bf
chaseweaver <chase.weaver34@gmail.com>
2018-03-16 23:36:49 -0400

    null fix

--- commands/Music/join.js
+++ commands/Music/join.js
@@ -25,14 +25,6 @@ module.exports = class extends Command {
       const { voiceChannel } = msg.member;
       if (!voiceChannel) return msg.send('You are not conected in a voice channel!');
       await voiceChannel.join();
-
-      const handler = msg.client.queue.get(msg.guild.id);
-      if (!handler || !handler.playing) {
-        setTimeout(() => {
-          if (!handler || !handler.playing) return msg.member.voiceChannel.leave();
-        }, 1000 * 60 * 30);
-      }
-
       return msg.send(`Connected to the voice channel \`${voiceChannel}\`.`).then((m) => { m.delete(5000); msg.delete(); });
     } catch (err) { console.log(err); }
   }
