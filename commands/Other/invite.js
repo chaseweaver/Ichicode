7ab0b900df6669fcc1bd04cc8c39216e6c23db12
chaseweaver <chase.weaver34@gmail.com>
2018-03-18 21:38:07 -0400

    Updated info

--- commands/Other/invite.js
+++ commands/Other/invite.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 /* Intended to override Klasa default. */
 
-=======
->>>>>>> dev
 const { Command } = require('klasa');
 
 module.exports = class extends Command {
@@ -11,23 +8,14 @@ module.exports = class extends Command {
       name: 'invite',
       enabled: false,
       runIn: ['text'],
-<<<<<<< HEAD
       cooldown: 0,
-=======
-      cooldown: 2,
->>>>>>> dev
       bucket: 1,
       aliases: [],
       permLevel: 0,
       botPerms: [],
       requiredConfigs: [],
-<<<<<<< HEAD
       description: 'Not yet available, sorry!',
       quotedStringSupport: true,
-=======
-      description: 'Not available!',
-      quotedStringSupport: false,
->>>>>>> dev
       usage: '',
       usageDelim: '',
       extendedHelp: 'No extended help available.',
