178346a1d5e7a54fef3d4120be4a4daf72e87210
chaseweaver <chase.weaver34@gmail.com>
2018-03-18 23:07:48 -0400

    Initial

---
+++ config.ex.js
@@ -0,0 +1,8 @@
+exports.master = 'YOUR_TOKEN_ID_HERE';
+exports.token = 'YOUR_BOT_TOKEN_HERE';
+exports.ytkey = 'YOUR_YOUTUBE_KEY_HERE';
+
+/* IDs for bot's test server. This allows users from guilds to report and request features. */
+exports.masterServer = 'YOUR_MASTER_BOT_SERVER_ID_HERE';
+exports.reportChannel = 'YOUR_MASTER_BOT_SERVER_REPORT_CHANNEL_ID_HERE';
+exports.requestChannel = 'YOUR_MASTER_BOT_SERVER_REQUEST_CHANNEL_ID_HERE';
