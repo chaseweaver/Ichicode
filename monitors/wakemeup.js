e397f031ac129f96c6c3ba20127c9c9d9285d1cb
chaseweaver <chase.weaver34@gmail.com>
2018-03-22 05:50:23 -0400

    initial

--- monitors/wakemeup.js
+++ monitors/wakemeup.js
@@ -13,12 +13,22 @@ module.exports = class extends Monitor {
   }
 
   run(msg) {
-    if (msg.content.length >= 50 || msg.content.length <= 5 || msg.author.id === this.client.user.id) return;
-    if (msg.content.match(/(\b[wW][aA][kK][eE]\b)/) && msg.content.match(/(\b[uU][pP]\b)/) &&
-      msg.content.toUpperCase().startsWith('WAKE')) {
-      const str = msg.content.replace(/(\b[wW][aA][kK][eE]\b)/, '');
-      const final = str.replace(/(\b[uU][pP]\b)/, '');
-      return msg.send(`*Wakes${final}up inside!*`);
+    if (!msg.guild.configs.wakemeupMonitor || !msg.guild.configs.monitorCooldown || msg.content.length > 50 || 
+      msg.content.length <= 5 || msg.author.id === this.client.user.id) return;
+    if (msg.content.toUpperCase().includes('WAKE ME UP INSIDE')) return msg.send(`*Wake me up inside!*`);
+    if (msg.content.toUpperCase().startsWith('WAKE') && msg.content.toUpperCase().endsWith('UP')) {
+      let active = true;
+      msg.channel.messages.fetch({ limit: 100 })
+        .then(m => {
+          const arr = m.array();
+          for (let i = 0; i < arr.length; i++) {
+            if ((arr[i].content.toUpperCase().startsWith('WAKE') || arr[i].content.toUpperCase().endsWith('UP')) &&
+              msg.createdTimestamp <= (arr[i].createdTimestamp + msg.guild.configs.monitorCooldown * 1000))
+              return active = false;
+          }
+        })
+        .then(function() { if (!active) return msg.send(`*Wakes${msg.content.substring(4, msg.content.length - 2)}up inside!*`); })
+        .catch(console.error);
     }
   }
 };
