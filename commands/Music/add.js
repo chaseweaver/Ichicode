95ab2c0a53c0706b5a96accbd0fa7bd6b49b2ca6
chaseweaver <chase.weaver34@gmail.com>
2018-03-22 05:53:45 -0400

    Changed trim, fixed time (hopefully)

--- commands/Music/add.js
+++ commands/Music/add.js
@@ -74,7 +74,7 @@ module.exports = class extends Command {
           url: info.video_url,
           title: info.title,
           seconds: info.length_seconds,
-          length: await moment.duration(info.length_seconds * 1000).format('h:mm:ss', { trim: false }),
+          length: await moment.duration(info.length_seconds * 1000).format('h:mm:ss', { trim: true }),
           thumbnail: info.thumbnail_url,
           requester: msg.author.username,
           requesterID: msg.author.id,
@@ -83,17 +83,17 @@ module.exports = class extends Command {
 
         const handler = this.client.queue.get(msg.guild.id);
         let total = 0, totalTime = 0;
-        for (let i = 1; i < Math.min(handler.songs.length); i++) { total += parseInt(handler.songs[i].seconds); }
+        for (let i = 0; i < Math.min(handler.songs.length); i++) { total += parseInt(handler.songs[i].seconds); }
         if (handler.songs.length == 1) total = 'NOW';
-        else if (handler.songs.length > 1 && msg.guild.voiceConnection && handler.playing) total += ((handler.songs[0].seconds * 1000) - msg.guild.voiceConnection.dispatcher.streamTime);
-        totalTime = (total === 'NOW' ? 'NOW' : await moment.duration(total).format('h:mm:ss', { trim: false }));
+        else if (handler.songs.length > 1 && msg.guild.voiceConnection && handler.playing) total -= parseInt(((handler.songs[0].seconds * 1000) - msg.guild.voiceConnection.dispatcher.streamTime));
+        totalTime = (total === 'NOW' ? 'NOW' : await moment.duration(total).format('h:mm:ss', { trim: true }));
 
         const embed = new this.client.methods.Embed()
           .setColor('#ff003c')
           .setTitle(info.title)
           .setThumbnail(info.thumbnail_url)
           .setAuthor(msg.author.username, msg.author.displayAvatarURL())
-          .addField('Length', await moment.duration(info.length_seconds * 1000).format('h:mm:ss', { trim: false }), true)
+          .addField('Length', await moment.duration(info.length_seconds * 1000).format('h:mm:ss', { trim: true }), true)
           .addField('Position in Queue', msg.client.queue.get(msg.guild.id).songs.length, true)
           .addField('Estimated Time Until Playing', totalTime, true)
           .setURL(info.video_url)
@@ -107,6 +107,7 @@ module.exports = class extends Command {
     const id = this.exp.exec(song);
     if (id) return `https://youtu.be/${id[1]}`;
     const data = await fetchURL(encodeURIComponent(song));
+    console.log(data);
     const video = data.items.find(item => item.id.kind !== 'youtube#channel');
     return video ? `https://youtu.be/${video.id.videoId}` : null;
   }
