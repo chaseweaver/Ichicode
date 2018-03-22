abc1ac570a8fcddd4c952843e2748e380ac3b56b
chaseweaver <chase.weaver34@gmail.com>
2018-03-22 01:20:05 -0400

    Initial

--- commands/Guild/playing.js
+++ commands/Guild/playing.js
@@ -23,12 +23,17 @@ module.exports = class extends Command {
 
   async run(msg, [...game]) {
     const stat = [];
+    const pad = [];
     let ctr = 1;
     let str = game.join(' ');
-    stat.push(`== ${msg.guild.name} :: '${game}' ==`);
+
+    for (let i = 0; i < msg.guild.members.array().length; i++) { if (sim.compareTwoStrings(mem.user.presence.activity.name, str) >= 0.65) stat.push(mem.user.tag); }
+    const pctr = pad.sort((a, b) => a.length < b.length)[0].length;
+
+    stat.push(`== ${msg.guild.name} :: '${game}' ==\n`);
     await msg.guild.members.array().forEach(mem => {
       if (!mem.user.presence.activity || !mem.user.presence.activity.name) return;
-      if (sim.compareTwoStrings(mem.user.presence.activity.name, str) >= 0.65) stat.push(`${ctr <= 9 ? ' ' + ctr++ : ctr++}. :: ${mem.user.tag} is playing ${mem.user.presence.activity.name}`);
+      if (sim.compareTwoStrings(mem.user.presence.activity.name, str) >= 0.65) stat.push(`${ctr <= 9 ? ' ' + ctr++ : ctr++}. :: ${String(mem.user.tag).padEnd(pctr)} is playing ${mem.user.presence.activity.name}\n`);
     });
     if (stat.length <= 1) return msg.send(`I could not find any guild members playing \`${game}\``);
     else return msg.send(stat, { code: 'asciidoc', split: true });
