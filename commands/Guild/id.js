34c71e80b9571d24fd7402298b85b90a50fb3435
chaseweaver <chase.weaver34@gmail.com>
2018-03-22 03:18:53 -0400

    Initial

--- commands/Guild/id.js
+++ commands/Guild/id.js
@@ -22,35 +22,21 @@ module.exports = class extends Command {
 
   async run(msg, [type, member, ...value]) {
     value = value.length > 0 ? value.join(' ') : null;
-    let final;
     
     switch (type) {
     case 'guild':
-      return msg.send(`${msg.guild.name} / ${msg.guild.id}`, { code: 'xl' })
-        .catch(err => console.log(err, 'error'));
+      return msg.send(`${msg.guild.name} / ${msg.guild.id}`, { code: 'xl' }).catch(err => console.log(err, 'error'));
     case 'member':
-      if (member) {
-        return msg.send(`${member.user.tag} / ${member.id}`, { code: 'xl' })
-          .catch(err => console.log(err, 'error'));
-      }
+      if (member) return msg.send(`${member.user.tag} / ${member.id}`, { code: 'xl' }).catch(err => console.log(err, 'error'));
       break;
     case 'role':
-      if (msg.guild.roles.find('name', value)) {
-        final = msg.guild.roles.find('name', value);
-        return msg.send(`${final.name} / ${final.id}`, { code: 'xl' })
-          .catch(err => console.log(err, 'error'));
-      }
+      if (msg.guild.roles.find('name', value)) return msg.send(`${msg.guild.channels.find('name', value).name} / ${msg.guild.channels.find('name', value).id}`, { code: 'xl' }).catch(err => console.log(err, 'error'));
       break;
     case 'channel':
-      if (msg.guild.channels.find('name', value)) {
-        final = msg.guild.channels.find('name', value);
-        return msg.send(`#${final.name} / ${final.id}`, { code: 'xl' })
-          .catch(err => console.log(err, 'error'));
-      }
+      if (msg.guild.channels.find('name', value)) return msg.send(`#${msg.guild.channels.find('name', value).name} / ${msg.guild.channels.find('name', value).id}`, { code: 'xl' }).catch(err => console.log(err, 'error'));
       break;
     default:
-      return msg.send(`I cannot find the ID of \`${value}\``)
-        .catch(err => console.log(err, 'error'));
+      return msg.send(`I cannot find the ID of \`${value}\``).catch(err => console.log(err, 'error'));
     }
   }
 };
