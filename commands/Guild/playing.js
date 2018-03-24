504e0bb51f978c11ab202e7e28e2d501950df92b
chaseweaver <chase.weaver34@gmail.com>
2018-03-24 02:20:49 -0400

    Changed timing

--- commands/Guild/playing.js
+++ commands/Guild/playing.js
@@ -44,12 +44,23 @@ module.exports = class extends Command {
       if (sim.compareTwoStrings(mem.user.presence.activity.name, str) >= 0.65) {
         let time = '';
         if (mem.user.presence.activity.timestamps)
-          time = `for ${moment.duration(msg.createdTimestamp - mem.user.presence.activity.timestamps.start).format('h:mm:ss', { trim: true })}`;
+          time = `for ${this.fmtHMS((msg.createdTimestamp - mem.user.presence.activity.timestamps.start) / 1000)}`;
         stat.push(`${ctr <= 9 ? ' ' + ctr++ : ctr++}. :: ${String(mem.user.tag).padEnd(pctr)} is playing ${mem.user.presence.activity.name} ${time}\n`);
       }
         
     });
     if (stat.length <= 1) return msg.send(`I could not find any guild members playing \`${game}\``);
     else return msg.send(stat, { code: 'asciidoc', split: true });
+  }
+
+  fmtHMS(sec) {
+    let seconds = Math.floor(sec), hours = Math.floor(seconds / 3600);
+    seconds -= hours * 3600;
+    let minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+    if (hours   < 10) { hours   = '0' + hours; }
+    if (minutes < 10) { minutes = '0' + minutes; }
+    if (seconds < 10) { seconds = '0' + seconds; }
+    return hours + 'h ' + minutes + 'm ' + seconds + 's';
   }
 };
