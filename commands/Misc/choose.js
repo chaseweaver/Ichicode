e2cd4cd31c54d44c786ac6aaba1c456263d75040
chaseweaver <chase.weaver34@gmail.com>
2018-03-24 03:45:40 -0400

    Initial

---
+++ commands/Misc/choose.js
@@ -0,0 +1,29 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      name: 'choose',
+      enabled: true,
+      runIn: ['text', 'dm', 'group'],
+      cooldown: 2,
+      bucket: 1,
+      aliases: ['pick', 'select'],
+      permLevel: 0,
+      botPerms: [],
+      requiredConfigs: [],
+      description: 'Let me choose between some options for you!',
+      quotedStringSupport: true,
+      usage: '<option1:str> <option2:str> [...]',
+      usageDelim: ' | ',
+      extendedHelp: 'No extended help available.',
+    });
+  }
+
+  async run(msg, [option1, option2, ...ext]) {
+    const opt = [option1, option2];
+    ext.slice(' | ').forEach(e => { opt.push(e); });
+    const src = [Math.floor(Math.random() * opt.length)];
+    return msg.send(`I choose **${opt[src]}**!`);
+  }
+};
