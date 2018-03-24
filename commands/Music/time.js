2b7127dcc3ab7fed409103055ea6b6336f044f06
chaseweaver <chase.weaver34@gmail.com>
2018-03-24 02:29:05 -0400

    Fixed timing

--- commands/Music/time.js
+++ commands/Music/time.js
@@ -26,6 +26,18 @@ module.exports = class extends Command {
     if (!msg.guild.voiceConnection) throw 'I am not connected in a voice channel!';
     const handler = this.client.queue.get(msg.guild.id);
     if (!handler || handler.playing === false) throw 'I am not playing music.';
-    return msg.send(`Time remaining: ${moment.duration((handler.songs[0].seconds * 1000) - msg.guild.voiceConnection.dispatcher.streamTime).format('h:mm:ss', { trim: false })}`);
+    const final = !handler.songs[0].upload ? `${this.fmtHMS(((handler.songs[0].seconds * 1000) - msg.guild.voiceConnection.dispatcher.streamTime) / 1000)}` : 'N/A';
+    return msg.send(`Time remaining: ${final}`);
+  }
+
+  fmtHMS(sec) {
+    let seconds = Math.floor(sec), hours = Math.floor(seconds / 3600);
+    seconds -= hours * 3600;
+    let minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+    if (hours   < 10) { hours   = '0' + hours; }
+    if (minutes < 10) { minutes = '0' + minutes; }
+    if (seconds < 10) { seconds = '0' + seconds; }
+    return hours + 'h ' + minutes + 'm ' + seconds + 's';
   }
 };
