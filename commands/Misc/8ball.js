ec69af448ac60db5a66f64cd421b39ed86d01b18
chaseweaver <chase.weaver34@gmail.com>
2018-03-26 18:18:19 -0400

    Changed responses

--- commands/Misc/8ball.js
+++ commands/Misc/8ball.js
@@ -1,4 +1,5 @@
 const { Command } = require('klasa');
+const figletAsync = require('util').promisify(require('figlet'));
 
 module.exports = class extends Command {
   constructor(...args) {
@@ -22,39 +23,37 @@ module.exports = class extends Command {
 
   async run(msg, [quest]) {
     return msg.reply(quest.endsWith('?') ?
-      `🎱 ${answers[Math.floor(Math.random() * answers.length)]}` :
-      '🎱 That doesn\'t look like a question, try again please.');
+			`🎱 ${answers[Math.floor(Math.random() * answers.length)]}` :
+			'🎱 That doesn\'t look like a question, try again please.');
   }
 };
 
 const answers = [
-  'Maybe.',
-  'Certainly not.',
-  'I hope so.',
-  'Not in your wildest dreams.',
-  'There is a good chance.',
-  'Quite likely.',
-  'I think so.',
-  'I hope not.',
-  'I hope so.',
-  'Never!',
-  'Fuhgeddaboudit.',
-  'Ahaha! Really?!?',
-  'Pfft.',
-  'Sorry, bucko.',
-  'Hell, yes.',
-  'Hell to the no.',
-  'The future is bleak.',
-  'The future is uncertain.',
-  'I would rather not say.',
-  'Who cares?',
-  'Possibly.',
-  'Never, ever, ever.',
-  'There is a small chance.',
-  'Yes!',
-  'lol no.',
-  'There is a high probability.',
-  'What difference does it make?',
-  'Not my problem.',
-  'Ask someone else.',
+	'Maybe.',
+	'Certainly not.',
+	'I hope so.',
+	'Not in your wildest dreams.',
+	'There is a good chance.',
+	'Quite likely.',
+	'I think so.',
+	'I hope not.',
+	'I hope so.',
+	'Never!',
+	'Pfft.',
+	'Sorry, bucko.',
+	'Hell, yes.',
+	'Hell to the no.',
+	'The future is bleak.',
+	'The future is uncertain.',
+	'I would rather not say.',
+	'Who cares?',
+	'Possibly.',
+	'Never, ever, ever.',
+	'There is a small chance.',
+	'Yes!',
+	'lol no.',
+	'There is a high probability.',
+	'What difference does it makes?',
+	'Not my problem.',
+	'Ask someone else.'
 ];
