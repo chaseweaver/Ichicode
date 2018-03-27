77bb91a78de808f1128fa331e915726ae445b941
chaseweaver <chase.weaver34@gmail.com>
2018-03-26 20:12:52 -0400

    Fixed

--- monitors/iagree.js
+++ monitors/iagree.js
@@ -14,6 +14,6 @@ module.exports = class extends Monitor {
 
   run(msg) {
     if (!msg.guild.configs.iagreeMonitor || msg.content.length > 30 || msg.content.length !== 1 || msg.author.id === this.client.user.id) return;
-    if (msg.content === '^' && msg.channel.messages.fetch({ before: msg.id, limit: 1 }).then(m => { if (m.first().content !== '^' || m.author.id !== this.client.user.id) return msg.send('I agree!') }));
+    if (msg.content === '^' && msg.channel.messages.fetch({ before: msg.id, limit: 1 }).then(m => { if (m.first().content !== '^' && m.author.id !== this.client.user.id) return msg.send('I agree!') }));
   }
 };
