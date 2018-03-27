029e0804dadb82919ab03a856451ab7b4335e5ab
chaseweaver <chase.weaver34@gmail.com>
2018-03-27 01:07:24 -0400

    Added I AM

--- monitors/dadjoke.js
+++ monitors/dadjoke.js
@@ -15,7 +15,7 @@ module.exports = class extends Monitor {
   run(msg) {
     if (!msg.guild.configs.dadjokeMonitor || msg.content.length > 30 || msg.content.length <= 5 || msg.author.id === this.client.user.id) return;
     if (Math.floor(Math.random() * 31) !== 0) return;
-    if ((msg.content.toUpperCase().startsWith('IM') || msg.content.toUpperCase().startsWith('I\'M')) && msg.author.id !== this.client.user.id) {
+    if ((msg.content.toUpperCase().startsWith('IM') || msg.content.toUpperCase().startsWith('I\'M') || msg.content.toUpperCase().startsWith('I AM')) && msg.author.id !== this.client.user.id) {
       if (msg.content.toUpperCase().startsWith('IM')) return msg.send(`Hi,${msg.content.substr(2)}, I'm Ichicode!`);
       if (msg.content.toUpperCase().startsWith('I\'M')) return msg.send(`Hi,${msg.content.substr(3)}, I'm Ichicode!`);
     }
