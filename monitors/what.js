8e6f747d3a3ddd15be0e63942366cceb90a65dd0
chaseweaver <chase.weaver34@gmail.com>
2018-03-26 20:05:07 -0400

    Fixed

--- monitors/what.js
+++ monitors/what.js
@@ -16,7 +16,7 @@ module.exports = class extends Monitor {
     if (!msg.content || !msg.guild.configs.whatMonitor || msg.content.length >= 5 || msg.author.id === this.client.user.id) return;
     if (Math.floor(Math.random() * 6) !== 0) return;
     else if ((msg.content.toUpperCase().startsWith('WHAT') || msg.content.toUpperCase().startsWith('WAT') || msg.content.toUpperCase().startsWith('WUT')) && msg.author.id !== this.client.user.id) {
-      msg.channel.messages.fetch({ before: msg.id, limit: 1 }).then(m => { console.log(m.first().content); return msg.send(`**${m.first().content.toUpperCase()}**`) });
+      msg.channel.messages.fetch({ before: msg.id, limit: 1 }).then(m => { return msg.send(`**${m.first().content.toUpperCase()}**`) });
     }
   }
 };
