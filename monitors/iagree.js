9449405bee181bbac6c6437d37a5f30be11d1b41
chaseweaver <chase.weaver34@gmail.com>
2018-03-26 21:43:33 -0400

    Incresed roulette

--- monitors/iagree.js
+++ monitors/iagree.js
@@ -14,7 +14,7 @@ module.exports = class extends Monitor {
 
   run(msg) {
     if (!msg.guild.configs.iagreeMonitor || msg.content.length !== 1 || msg.author.id === this.client.user.id) return;
-    if (Math.floor(Math.random() * 6) !== 0) return;
+    if (Math.floor(Math.random() * 31) !== 0) return;
     if (msg.content === '^') msg.channel.messages.fetch({ before: msg.id, limit: 1 }).then(m => { if (m.first().content !== '^') return msg.send('I agree!') });
   }
 };
--- monitors/what.js
+++ monitors/what.js
@@ -14,7 +14,7 @@ module.exports = class extends Monitor {
 
   run(msg) {
     if (!msg.content || !msg.guild.configs.whatMonitor || msg.content.length >= 5 || msg.author.id === this.client.user.id) return;
-    if (Math.floor(Math.random() * 6) !== 0) return;
+    if (Math.floor(Math.random() * 31) !== 0) return;
     else if ((msg.content.toUpperCase() === 'WHAT' || msg.content.toUpperCase() === 'WAT' || msg.content.toUpperCase() === 'WUT') && msg.author.id !== this.client.user.id) {
       msg.channel.messages.fetch({ before: msg.id, limit: 1 }).then(m => { return msg.send(`**${m.first().content.toUpperCase()}**`) });
     }
