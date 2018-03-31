2ba835ac0b4b292032069551593caa3047f6c345
chaseweaver <chase.weaver34@gmail.com>
2018-03-31 08:27:50 -0400

    Prevents WAKE UP

--- monitors/wakemeup.js
+++ monitors/wakemeup.js
@@ -14,7 +14,7 @@ module.exports = class extends Monitor {
 
   run(msg) {
     if (!msg.content || !msg.guild.configs.wakemeupMonitor || msg.content.length > 50 ||  msg.content.length <= 5 || msg.author.id === this.client.user.id) return;
-    if (msg.content.toUpperCase().startsWith('WAKE ME UP INSIDE')) return msg.send(`*Can't wake up!*`);
-    if (msg.content.toUpperCase().startsWith('WAKE') && msg.content.toUpperCase().endsWith('UP')) return msg.send(`*Wakes${msg.content.substring(4, msg.content.length - 2)}up inside!*`);
+    if (msg.content.toUpperCase() === 'WAKE ME UP INSIDE') return msg.send(`*Can't wake up!*`);
+    if (msg.content.toUpperCase().startsWith('WAKE') && msg.content.toUpperCase().endsWith('UP') && msg.content.length !== 7) return msg.send(`*Wakes${msg.content.substring(4, msg.content.length - 2)}up inside!*`);
   }
 };
