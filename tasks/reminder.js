2b5b0b418303b78c201857cec422fde84f04f699
chaseweaver <chase.weaver34@gmail.com>
2018-03-31 02:30:21 -0400

    Added return

--- tasks/reminder.js
+++ tasks/reminder.js
@@ -2,6 +2,7 @@ const { Task } = require('klasa');
 
 module.exports = class extends Task {
 	async run({ member, text }) {
+		if (!member || !text) return;
 		return member.send(`You wanted me to remind you: \`${text}\``);
 	}
 };
--- tasks/removeBirthday.js
+++ tasks/removeBirthday.js
@@ -2,6 +2,7 @@ const { Task } = require('klasa');
 
 module.exports = class extends Task {
 	async run({ member, role }) {
+		if (!member || !role) return;
 		await member.roles.remove(role).catch(error => console.log(error));
 	}
 };
