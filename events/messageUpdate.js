3c43c2da6935f0c31e01cead16ac96f331a7f555
chaseweaver <chase.weaver34@gmail.com>
2018-04-07 01:31:55 -0400

    Fixed null

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -11,7 +11,7 @@ module.exports = class extends Event {
   }
 
   run(oldMsg, newMsg) {
-    if (oldMsg.channel.type !== 'text' || msg.author.bot) return;
+    if (oldMsg.channel.type !== 'text' || oldMsg.author.bot) return;
     try {
       if (oldMsg.guild.configs.logMessageEdit && oldMsg.guild.configs.messageEditChannel) {
         const chan = oldMsg.guild.channels.find('id', oldMsg.guild.configs.messageEditChannel);
