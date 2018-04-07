eb50fffcb5a7da737042a84aed49009e1c26b7f1
chaseweaver <chase.weaver34@gmail.com>
2018-04-07 01:40:42 -0400

    Fixed null

--- commands/Other/report.js
+++ commands/Other/report.js
@@ -26,7 +26,7 @@ module.exports = class extends Command {
       if (!chan) return;
       const embed = new msg.client.methods.Embed()
         .setColor('#ff003c')
-        .addField('Request', message.length > 0 ? message : message.join(' '))
+        .addField('Report', message.length > 0 ? message : message.join(' '))
         .setTimestamp();
       return msg.guild.channels.find('id', chan).sendEmbed(embed)
         .then(msg.delete())
