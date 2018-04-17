d73f92c246bd9a165184e2c8928ed9e5f437fc65
chaseweaver <chase.weaver34@gmail.com>
2018-04-16 21:27:05 -0400

    Initial

---
+++ commands/Guild/nsfw.js
@@ -0,0 +1,26 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      name: 'nsfw',
+      enabled: true,
+      runIn: ['text'],
+      cooldown: 2,
+      bucket: 1,
+      aliases: [''],
+      permLevel: 5,
+      botPerms: [],
+      requiredConfigs: [],
+      description: 'Returns NSFW block message.',
+      quotedStringSupport: false,
+      usage: '',
+      usageDelim: '',
+      extendedHelp: null,
+    });
+  }
+
+  async run(msg) {
+    return msg.send(`Assuming you have a job and your boss walks behind you and looks at your screen, would you be okay with them seeing this kind of content? Thank you for understanding.`, { code: 'txt' });
+  }
+};
