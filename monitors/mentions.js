9d43717e6fa7013116b808004122c70c8837e320
chaseweaver <chase.weaver34@gmail.com>
2018-04-19 21:11:07 -0400

    QoL

--- monitors/mentions.js
+++ monitors/mentions.js
@@ -1,5 +1,5 @@
 const { Monitor } = require('klasa');
-const { masterServer, dmChannel } = require('../config.js');
+const { masterServer, mentionsChannel } = require('../config.js');
 
 module.exports = class extends Monitor {
   constructor(...args) {
@@ -14,16 +14,16 @@ module.exports = class extends Monitor {
   }
 
   run(msg) {
-    if (msg.channel.type !== 'dm' || msg.author.id === this.client.owner.id) return;
+    if (msg.channel.type !== 'text' || msg.content.includes(`<@!${this.client.id}>`)) return;
     try {
-      if (!masterServer || !dmChannel) return;
+      if (!masterServer || !mentionsChannel) return;
       const embed = new msg.client.methods.Embed()
         .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
-        .setAuthor(`${msg.author.tag} / ${msg.author.id}`, msg.author.displayAvatarURL())
-        .setThumbnail(msg.author.displayAvatarURL())
+        .setAuthor(`${msg.author.tag} / ${msg.author.user.id}`, msg.author.user.displayAvatarURL())
+        .setThumbnail(msg.author.user.displayAvatarURL())
+        .addField('❯ Guild', `${msg.guild.name} [${msg.guild.memberCount}] / ${msg.guild.id}`)
         .addField('❯ Message', msg.content)
-        .setTimestamp();
-      return this.client.guilds.find('id', masterServer).channels.find('id', dmChannel)
+      return this.client.guilds.find('id', masterServer).channels.find('id', mentionsChannel)
         .sendEmbed(embed).catch(err => msg.client.emit('log', err, 'error'));
     } catch (err) { console.log(err); }
   }
