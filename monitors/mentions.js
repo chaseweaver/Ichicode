abb012207340cccbbe70e7b5bad47b6ae3297769
chaseweaver <chase.weaver34@gmail.com>
2018-04-19 21:25:53 -0400

    Fixed null

--- monitors/mentions.js
+++ monitors/mentions.js
@@ -14,17 +14,19 @@ module.exports = class extends Monitor {
   }
 
   run(msg) {
-    if (msg.channel.type !== 'text' || !msg.content.includes(`<@!${this.client.user.id}>`)) return;
+    if (msg.channel.type !== 'text') return;
     try {
       if (!masterServer || !mentionsChannel) return;
-      const embed = new msg.client.methods.Embed()
-        .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
-        .setAuthor(`${msg.author.tag} / ${msg.author.id}`, msg.author.displayAvatarURL())
-        .setThumbnail(msg.author.displayAvatarURL())
-        .addField('❯ Guild', `${msg.guild.name} [${msg.guild.memberCount}] / ${msg.guild.id}`)
-        .addField('❯ Message', msg.content)
-      return this.client.guilds.find('id', masterServer).channels.find('id', mentionsChannel)
-        .sendEmbed(embed).catch(err => msg.client.emit('log', err, 'error'));
+      if (msg.content.includes(msg.mentions.has(this.client.user))) {
+        const embed = new msg.client.methods.Embed()
+          .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
+          .setAuthor(`${msg.author.tag} / ${msg.author.id}`, msg.author.displayAvatarURL())
+          .setThumbnail(msg.author.displayAvatarURL())
+          .addField('❯ Guild', `${msg.guild.name} [${msg.guild.memberCount}] / ${msg.guild.id}`)
+          .addField('❯ Message', msg.content)
+        return this.client.guilds.find('id', masterServer).channels.find('id', mentionsChannel)
+          .sendEmbed(embed).catch(err => msg.client.emit('log', err, 'error'));
+      }
     } catch (err) { console.log(err); }
   }
 };
